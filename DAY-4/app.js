dc6234bd6e1ac44877654f688f845698500dea13
dhruv0456 <dhruvpatodia801@gmail.com>
2025-03-07 11:07:21 -0600

    practice work

---
+++ DAY-4/app.js
@@ -0,0 +1,29 @@
+let a = prompt("Enter A character")
+
+switch (a) {
+    case "A":
+        alert("Vowels");
+        break;
+
+    case "E":
+        alert("Vowels");
+        break;
+
+    case "I":
+        alert("Vowels");
+        break;
+
+    case "O":
+        alert("Vowels");
+        break;
+
+    case "U":
+        alert("Vowels");
+        break;
+
+    default:
+        alert("This Character is not vowels")
+
+
+
+}
---
+++ projects js/practice work project js/app.js
@@ -0,0 +1,163 @@
+// let a = prompt ("Enter: \n  1 for ordering pizza \n  2 for burger \n 3 for both");
+
+// console.log(typeof(a));
+
+// if (a == 1){
+//     console.log("you ordered a pizza 🍕");
+    
+// } else {
+//     console.log("you ordered a burger 🍔");
+    
+// }
+
+// if (a == 1){
+//     console.log("you ordered a pizza");
+//     } else if(a == 2) {
+//         console.log("you ordered a burger");
+        
+//     } else if (a == 3) {
+//         console.log("you ordered both ");  
+
+//     } else {
+//         console.log("you cancel ordered ❌");
+        
+//     }
+
+
+
+
+// let a = 20;
+// let b = 60;
+// let c
+
+// console.log("a is",a);
+// console.log("b is",b);
+
+// a=a+b
+// b=a-b
+// a=a-b
+
+// console.log("after swapping value");
+// console.log("a is",a);
+// console.log("b is",b);
+
+
+
+
+// let age = 20;
+
+// if (age >= 18) {
+//     if (age <= 17) {
+//         console.log("You can drive.");
+//     } else {
+//         console.log("You can't drive.");
+//     }
+// } else {
+//     console.log("You are too young to drive.");
+// }
+
+
+
+
+
+
+
+// let a = 10;
+// let b = 10;
+// let c = 10;
+
+// if (a <= b) {
+//     if (a <= c) {
+//         console.log("a is min");
+        
+//     }else {
+//         console.log("c is min");
+        
+//     }
+// }else {
+//     if (b <= c) {
+//         console.log("b is min");
+//     }else {
+//         console.log("c is min");
+//     }
+// }
+
+
+
+
+
+// let a = 12;
+// let b = 35;
+// let c;
+
+// c = a;
+// a = b;
+// b = c;
+
+// console.log ("a =", a, "b =", b);
+
+// let a = 12;
+// let b = 35;
+
+// [a,b] = [b,a];
+
+// console.log(a,b);
+
+
+
+
+
+// let a = prompt("Enter A Chearter");
+
+
+// switch (a) {
+//     case "A":
+//         alert("Vowels");
+//         break;
+
+//     case "E":
+//         alert("Vowels");
+//         break;
+
+//     case "I":
+//         alert("Vowels");
+//         break;
+
+//     case "O":
+//         alert("Vowels");
+//         break;
+
+//     case "U":
+//         alert("Vowels");
+//         break;
+
+//     case "a":
+//         alert("Vowels");
+//         break;
+
+//     case "e":
+//         alert("Vowels");
+//         break;
+
+//     case "i":
+//         alert("Vowels");
+//         break;
+
+//     case "o":
+//         alert("Vowels");
+//         break;
+
+//     case "u":
+//         alert("Vowels");
+//         break;
+
+//     default :
+//         alert ("Not vowels")
+    
+
+// }
+
+
+
+
+
