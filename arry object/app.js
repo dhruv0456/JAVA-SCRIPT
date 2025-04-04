e827cc3accb22cfc9cbf849b27bb71571b1f3eed
dhruv0456 <dhruvpatodia801@gmail.com>
2025-04-03 20:19:05 -0600

    Project-4

---
+++ arry object/app.js
@@ -0,0 +1,13 @@
+let arr = [
+    {id:1,name:"Rahul",subject:"react"},
+    {id:1,name:"sumit",subject:"react"},
+    {id:1,name:"puneet",subject:"react"},
+]
+
+let arr2 = arr.map((e,i)=>{
+    return e;
+})
+
+console.log(arr2);
+
+
